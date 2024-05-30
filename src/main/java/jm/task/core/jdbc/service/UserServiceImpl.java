df67f5242a909e51ee0f4cc3f9ce3c504f052c63
Ivn665 <157416939+Ivn665@users.noreply.github.com>
2024-05-30 18:10:03 +0300

    Слил все ветки

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -11,8 +11,8 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private final UserDao userDao = new UserDaoJDBCImpl();
-    //private final UserDao userDao = new UserDaoHibernateImpl();
+    //private final UserDao userDao = new UserDaoJDBCImpl();
+    private final UserDao userDao = new UserDaoHibernateImpl();
 
     public void createUsersTable() throws SQLException, HibernateException {
         userDao.createUsersTable();
